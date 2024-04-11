4417d8eeecbf6ab552a929175089a3922307cc61
InfoTechweave <info.techweaveinfotech@gmail.com>
2024-04-11 10:21:55 +0530

    home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,55 +1,415 @@
 <!DOCTYPE html>
 <html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
 
-<body>
-    <?php
+    <link rel="stylesheet" href="{{ asset('css/home/global.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/home/index.css') }}">
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,600&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Helvetica Neue:wght@700&display=swap"
+    />
+  </head>
+  <body>
+    <div class="my-profile">
+      <div class="frame-parent">
+        <div class="frame-group">
+          <div class="rectangle-parent">
+            <div class="frame-child"></div>
+            <button class="add-cash-label">
+              <div class="add-cash-label-child"></div>
+              <div class="add-cash-wrapper">
+                <div class="add-cash">Add Cash</div>
+              </div>
+              <div class="vector-parent">
+                <img class="frame-item" alt="" src="{{ asset('img/home/vector-2.svg') }}" />
+                <img class="wallet-icon" alt="" src="{{ asset('img/home/wallet@2x.png') }}" />
+              </div>
+            </button>
+            <div class="polygon-parent">
+              <img class="frame-inner" alt="" src="{{ asset('img/home/polygon-1.svg') }}" />
+              <img class="polygon-icon" alt="" src="{{ asset('img/home/polygon-2.svg') }}" />
+              <img class="close-icon" loading="lazy" alt="" src="{{ asset('img/home/close.svg') }}" />
+            </div>
+            <img class="icon" alt="" src="{{ asset('img/home/1-10@2x.png') }}"  />
+          </div>
+          <div class="frame-wrapper">
+            <div class="polygon-group">
+              <img class="frame-child1" alt="" src="{{ asset('img/home/polygon-3.svg') }}" />
 
-/* After successful phone number verification an access_token will be returned by auth verification popup. In this if condition  please use this access_token to call Phone Email API to get verified phone number.  */
-if(isset($_GET['access_token'])){
+              <img
+                class="discount-icon"
+                loading="lazy"
+                alt=""
+                src="{{ asset('img/home/discount@2x.png') }}"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="game-section-wrapper">
+          <div class="game-section">
+            <img
+              class="game-section-child"
+              alt=""
+              src="{{ asset('img/home/rectangle-12.svg') }}"
+            />
 
-    /* To get verified phone number please call the getuser API */
+            <div class="game-section-inner">
+              <div class="frame-container">
+                <div class="bonus-summary-label-wrapper">
+                  <div class="bonus-summary-label">
+                    <div class="cricket-wrapper">
+                      <img
+                        class="cricket-icon"
+                        loading="lazy"
+                        alt=""
+                        src="{{ asset('img/home/cricket@2x.png') }}"
+                      />
+                    </div>
+                    <div class="cricket">Cricket</div>
+                  </div>
+                </div>
+                <div class="how-to-play-label">
+                  <div class="legality-label">
+                    <img
+                      class="sport-day-3-converted-1"
+                      loading="lazy"
+                      alt=""
+                      src="{{ asset('img/home/sport-day-3-converted-1@2x.png') }}"
+                    />
+                  </div>
+                  <div class="kabaddi">Kabaddi</div>
+                </div>
+                <div class="frame-div">
+                  <div class="soccer-ball-wrapper">
+                    <img
+                      class="soccer-ball-icon"
+                      loading="lazy"
+                      alt=""
+                      src="{{ asset('img/home/soccer-ball@2x.png') }}"
+                    />
+                  </div>
+                  <div class="football">Football</div>
+                </div>
+                <div class="dicons-set">
+                  <div class="basketball-wrapper">
+                    <img
+                      class="basketball-icon"
+                      loading="lazy"
+                      alt=""
+                      src="{{ asset('img/home/basketball@2x.png') }}"
+                    />
+                  </div>
+                  <div class="basketball">Basketball</div>
+                </div>
+              </div>
+            </div>
+            <div class="game-section-item"></div>
+            <div class="my-account-parent">
+              <div class="my-account">My Account</div>
+              <div class="line-div"></div>
+              <div class="view-transactions">View Transactions</div>
+              <div class="frame-child2"></div>
+              <div class="withdraw">Withdraw</div>
+              <div class="frame-child3"></div>
+              <div class="withdraw-history">Withdraw History</div>
+              <div class="frame-child4"></div>
+              <div class="bonus-summary">Bonus Summary</div>
+              <div class="frame-child5"></div>
+              <div class="kyc-verification">KYC Verification</div>
+              <div class="frame-child6"></div>
+              <div class="join-private-contest">Join Private Contest</div>
+              <div class="frame-child7"></div>
+              <div class="points-system">Points System</div>
+              <div class="frame-child8"></div>
+              <div class="how-to-play">How To Play</div>
+              <div class="frame-child9"></div>
+              <div class="legality">Legality</div>
+              <div class="frame-child10"></div>
+              <div class="terms-of-service">Terms Of Service</div>
+              <div class="frame-child11"></div>
+              <div class="about-us">About Us</div>
+              <div class="frame-child12"></div>
+              <div class="help">Help</div>
+              <div class="frame-child13"></div>
+              <div class="log-out">Log Out</div>
+            </div>
+          </div>
+        </div>
+        <div class="frame-wrapper1">
+          <div class="frame-parent1">
+            <div class="telecast-updates-wrapper">
+              <div class="telecast-updates">
+                <button class="rectangle-group">
+                  <div class="rectangle-div"></div>
+                  <div class="upcoming">Upcoming</div>
+                </button>
+                <div class="live-wrapper">
+                  <div class="live">Live</div>
+                </div>
+                <div class="completed-wrapper">
+                  <div class="completed">Completed</div>
+                </div>
+              </div>
+            </div>
+            <div class="team-section-1-parent">
+              <div class="team-section-1">
+                <img
+                  class="team-section-1-child"
+                  loading="lazy"
+                  alt=""
+                  src="{{ asset('img/home/ellipse-6@2x.png') }}"
+                />
 
-    // Initialize cURL session
-    $ch = curl_init();
-    $url = "https://eapi.phone.email/getuser";
-    $postData = array(
-        'access_token' => $_GET['access_token'],
-        'client_id' => '18373326995746576499'
-    );
+                <img
+                  class="team-section-1-item"
+                  loading="lazy"
+                  alt=""
+                  src="{{ asset('img/home/ellipse-7@2x.png') }}"
+                />
 
-    // Set cURL options
-    curl_setopt($ch, CURLOPT_URL, $url); // URL to submit the POST request
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return the response as a string instead of outputting it directly
-    curl_setopt($ch, CURLOPT_POST, true); // Set the request type to POST
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $postData); // Set the POST data
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Ignore SSL certificate verification (not recommended in production)
+                <b class="india">INDIA</b>
+                <i class="v">V</i>
+                <i class="s">S</i>
+                <b class="pakistan">PAKISTAN</b>
+                <div class="european-cricket-">
+                  European Cricket - Spain, T10
+                </div>
+                <input class="team-section-1-inner" type="text" />
 
-    $response = curl_exec($ch);
+                <div class="win-samsung-smartphone">
+                  Win Samsung Smartphone + ₹1 Lakh as 1st Prize
+                </div>
+                <div class="h-17m-2s">1h 17m 2s</div>
+                <div class="wrapper-3dicons">
+                  <img
+                    class="dicons"
+                    loading="lazy"
+                    alt=""
+                    src="{{ asset('img/home/3dicons@2x.png') }}"
+                  />
+                </div>
+              </div>
+              <div class="team-section-3">
+                <img
+                  class="team-section-3-child"
+                  loading="lazy"
+                  alt=""
+                  src="{{ asset('img/home/ellipse-6@2x.png') }}"
+                />
 
-    if ($response === false) {
-        // Handle error here
-    }
+                <img
+                  class="team-section-3-item"
+                  loading="lazy"
+                  alt=""
+                  src="{{ asset('img/home/ellipse-7@2x.png') }}"
+                />
 
-    curl_close($ch);
+                <b class="india1">INDIA</b>
+                <i class="v1">V</i>
+                <i class="s1">S</i>
+                <b class="pakistan1">PAKISTAN</b>
+                <div class="european-cricket-1">
+                  European Cricket - Spain, T10
+                </div>
+                <input class="team-section-3-inner" type="text" />
 
-    $json_data = json_decode($response,true);
+                <div class="win-samsung-smartphone1">
+                  Win Samsung Smartphone + ₹1 Lakh as 1st Prize
+                </div>
+                <div class="h-17m-2s1">1h 17m 2s</div>
+                <div class="wrapper-3dicons1">
+                  <img class="dicons1" alt="" src="{{ asset('img/home/3dicons@2x.png') }}" />
+                </div>
+              </div>
+              <div class="team-section-2">
+                <img
+                  class="team-section-2-child"
+                  loading="lazy"
+                  alt=""
+                  src="{{ asset('img/home/ellipse-6@2x.png') }}"
+                />
 
-    if($json_data['status'] != 200) {
-        // Handle error here
-    }
+                <img
+                  class="team-section-2-item"
+                  loading="lazy"
+                  alt=""
+                  src="{{ asset('img/home/ellipse-7@2x.png') }}"
+                />
 
-    $country_code = $json_data['country_code'];
-    $phone_no = $json_data['phone_no'];
-    echo $country_code ;
-    echo $phone_no;
-    ?>
-    <?php
-} ?>
+                <b class="india2">INDIA</b>
+                <i class="v2">V</i>
+                <i class="s2">S</i>
+                <b class="pakistan2">PAKISTAN</b>
+                <div class="european-cricket-2">
+                  European Cricket - Spain, T10
+                </div>
+                <input class="team-section-2-inner" type="text" />
 
-    <h1>My First Heading</h1>
+                <div class="win-samsung-smartphone2">
+                  Win Samsung Smartphone + ₹1 Lakh as 1st Prize
+                </div>
+                <div class="h-17m-2s2">1h 17m 2s</div>
+                <div class="wrapper-3dicons2">
+                  <img class="dicons2" alt="" src="{{ asset('img/home/3dicons@2x.png') }}" />
+                </div>
+              </div>
+              <div class="team-section-5">
+                <img
+                  class="team-section-5-child"
+                  alt=""
+                  src="{{ asset('img/home/ellipse-6@2x.png') }}"
+                />
 
-    <p>My first paragraph.</p>
+                <img
+                  class="team-section-5-item"
+                  alt=""
+                  src="{{ asset('img/home/ellipse-7@2x.png') }}"
+                />
 
-</body>
+                <b class="india3">INDIA</b>
+                <i class="v3">V</i>
+                <i class="s3">S</i>
+                <b class="pakistan3">PAKISTAN</b>
+                <div class="european-cricket-3">
+                  European Cricket - Spain, T10
+                </div>
+                <div class="team-section-5-inner"></div>
+                <div class="win-samsung-smartphone3">
+                  Win Samsung Smartphone + ₹1 Lakh as 1st Prize
+                </div>
+                <div class="h-17m-2s3">1h 17m 2s</div>
+                <div class="wrapper-3dicons3">
+                  <img class="dicons3" alt="" src="{{ asset('img/home/3dicons@2x.png') }}" />
+                </div>
+              </div>
+              <div class="team-section-6">
+                <img
+                  class="team-section-6-child"
+                  alt=""
+                  src="{{ asset('img/home/ellipse-6@2x.png') }}"
+                />
 
+                <img
+                  class="team-section-6-item"
+                  alt=""
+                  src="{{ asset('img/home/ellipse-7@2x.png') }}"
+                />
+
+                <b class="india4">INDIA</b>
+                <i class="v4">V</i>
+                <i class="s4">S</i>
+                <b class="pakistan4">PAKISTAN</b>
+                <div class="european-cricket-4">
+                  European Cricket - Spain, T10
+                </div>
+                <div class="team-section-6-inner"></div>
+                <div class="win-samsung-smartphone4">
+                  Win Samsung Smartphone + ₹1 Lakh as 1st Prize
+                </div>
+                <div class="h-17m-2s4">1h 17m 2s</div>
+                <div class="wrapper-3dicons4">
+                  <img class="dicons4" alt="" src="{{ asset('img/home/3dicons@2x.png') }}" />
+                </div>
+              </div>
+              <div class="team-section-7">
+                <img
+                  class="team-section-7-child"
+                  alt=""
+                  src="{{ asset('img/home/ellipse-6@2x.png') }}"
+                />
+
+                <img
+                  class="team-section-7-item"
+                  alt=""
+                  src="{{ asset('img/home/ellipse-7@2x.png') }}"
+                />
+
+                <b class="india5">INDIA</b>
+                <i class="v5">V</i>
+                <i class="s5">S</i>
+                <b class="pakistan5">PAKISTAN</b>
+                <div class="european-cricket-5">
+                  European Cricket - Spain, T10
+                </div>
+                <div class="team-section-7-inner"></div>
+                <div class="win-samsung-smartphone5">
+                  Win Samsung Smartphone + ₹1 Lakh as 1st Prize
+                </div>
+                <div class="h-17m-2s5">1h 17m 2s</div>
+                <div class="wrapper-3dicons5">
+                  <img class="dicons5" alt="" src="{{ asset('img/home/3dicons@2x.png') }}" />
+                </div>
+              </div>
+              <div class="team-section-4">
+                <img
+                  class="team-section-4-child"
+                  alt=""
+                  src="{{ asset('img/home/ellipse-6@2x.png') }}"
+                />
+
+                <img
+                  class="team-section-4-item"
+                  alt=""
+                  src="{{ asset('img/home/ellipse-7@2x.png') }}"
+                />
+
+                <b class="india6">INDIA</b>
+                <i class="v6">V</i>
+                <i class="s6">S</i>
+                <b class="pakistan6">PAKISTAN</b>
+                <div class="european-cricket-6">
+                  European Cricket - Spain, T10
+                </div>
+                <div class="team-section-4-inner"></div>
+                <div class="win-samsung-smartphone6">
+                  Win Samsung Smartphone + ₹1 Lakh as 1st Prize
+                </div>
+                <div class="h-17m-2s6">1h 17m 2s</div>
+                <div class="wrapper-3dicons6">
+                  <img class="dicons6" alt="" src="{{ asset('img/home/3dicons@2x.png') }}" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="frame-parent2">
+        <div class="rectangle-container">
+          <img
+            class="rectangle-icon"
+            alt=""
+            src="{{ asset('img/home/link-connector@2x.png') }}"
+          />
+
+          <img class="vignette-icon" alt="" src="{{ asset('img/home/vignette@2x.png') }}" />
+          
+          <img class="vignette-icon1" alt="" src="{{ asset('img/home/vignette@2x.png') }}" />
+        </div>
+        <img class="vignette-icon2" alt="" src="{{ asset('img/home/vignette@2x.png') }}" />
+      </div>
+    </div>
+  </body>
 </html>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+      const closeButton = document.querySelector('.close-icon');
+      const sideMenu = document.querySelector('.my-account-parent');
+
+      closeButton.addEventListener('click', function() {
+        // Toggle the class to show/hide the side menu
+        sideMenu.classList.toggle('hidden');
+      });
+    });
+</script>
+
+<style>
+    .my-account-parent.hidden {
+    display: none;
+}
+    </style>
